4be7af8bcb8fe44faec10e7476efcc8a2b26059a
turbopro <dantepoe@gmail.com>
2014-04-21 11:35:43 -0400

    Updated the comment text in the document to explain better the objective of the code
    Date: 04-21-2014
    By:   Vern

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,30 @@
+### R Programming - Programming Assignment 2
+
+### Date: 04-21-2014
+### By:   Vern
+### This code based on the 'makeVector' and 'cachemean' fucntions
+### Branched from: https://github.com/rdpeng/ProgrammingAssignment2
+
 ## The 'makeCacheMatrix' and 'cacheSolve' functions are used
 ## to: 
-##   1) create a special object that stores a matrix (invertible)
-##      to allow for caching of the inverse of the matrix
-##   2) cache the inverse of the matrix
+##   1) create a special object that stores a matrix (invertible) to
+##      allow for caching of the computed inverse of the matrix, and
+##   2) cache the computed inverse of the matrix
 ##
-## If the matrix does not change, then the the cached inverse
-## becomes availble for use later without the need to compute
-## the inverse afresh
+## Given that computing the inverse of a matrix may be computationally
+## expensive, the above two functions allow for the caching of the 
+## result for future use.  If the matrix does not change, then the
+## inverted matrix may be retrieved from the cached result
 
-## Note: we assume the matrix is always invertible
+## NB: we assume the matrix is always invertible
 
-## makeCacheMatrix:
-## Creates the special matrix object that is a list containing
-## the following four functions:
-##   set the value of the matrix
-##   get the value of the matrix
-##   set the value of the reversed (solved) matrix
-##   get the value of the reversed (solved) matrix
+#  makeCacheMatrix:
+#  Creates the special matrix object that is a list containing
+#  the following four functions:
+#    set the value of the matrix
+#    get the value of the matrix
+#    set the value of the inverted (solved) matrix
+#    get the value of the inverted (solved) matrix
 
 makeCacheMatrix <- function(x = matrix()) {
      m <- NULL
@@ -32,12 +40,12 @@ makeCacheMatrix <- function(x = matrix()) {
           getsolve = getsolve)
 }
 
-## cacheSolve:
-## Computes the inverse of the returned special matrix, which
-## contains the list of four functions 
-## If the inverse already has been calculated (and the matrix
-## has not changed), then the fucntion retrieves the inverse
-## from the cache
+#  cacheSolve:
+#  Computes the inverse of the returned special matrix, which
+#  contains the list of four functions 
+#  If the inverse already has been calculated (and the matrix
+#  has not changed), then the fucntion retrieves the inverse
+#  from the cache
 
 cacheSolve <- function(x, ...) {
      m <- x$getsolve()
